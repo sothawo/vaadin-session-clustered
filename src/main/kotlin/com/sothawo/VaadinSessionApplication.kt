4559bc82872f477795a8ced665829a3daf5752f0
P.J.Meisch <pj.meisch@sothawo.com>
2017-07-06 15:36:39 +0200

    added spring-session support with redis for vaadin

--- src/main/kotlin/com/sothawo/VaadinSessionApplication.kt
+++ src/main/kotlin/com/sothawo/VaadinSessionApplication.kt
@@ -2,9 +2,18 @@ package com.sothawo
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
+import org.vaadin.spring.session.redis.VaadinSessionRewriteFilter
 
 @SpringBootApplication
-class VaadinSessionApplication
+@EnableRedisHttpSession
+class VaadinSessionApplication {
+
+    @Bean
+    fun vaadinSessionRewriteFilter() = VaadinSessionRewriteFilter()
+
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(VaadinSessionApplication::class.java, *args)
